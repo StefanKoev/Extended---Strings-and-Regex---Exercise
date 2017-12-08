cd0a5e19ba848dad8328cfa1c84016197049b364
Stefan Koev <32458058+StefanKoev@users.noreply.github.com>
2017-12-08 19:29:24 +0200

    Create Traincode

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01.Traineram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"^(<\[[^A-Za-z0-9\n]*]\.)(\.\[[A-Za-z0-9]*]\.)*$";
+
+            while (input != "Traincode!")
+            {
+                if (Regex.IsMatch(input, pattern))
+                {
+                    Console.WriteLine(input);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
